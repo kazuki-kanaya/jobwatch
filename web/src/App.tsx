3f70e23a1ba0d697fa4427f19547d3cea8d6b5eb
Kazuki Kanaya Desktop <k.melon0106@gmail.com>
2026-02-05 17:41:06 +0900

    set up frontend

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import NotFoundPage from "./pages/NotFoundPage";
+import HomePage from "./pages/HomePage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
